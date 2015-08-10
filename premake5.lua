2b9192f752e9362672465fa199e08614052d5d1b
Sam Willis <samwill01@optimum.net>
2015-08-10 05:17:21 -0400

    Now using premake for project file generation;  Added project file extensions to .gitignore

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+solution "YAX.Math"
+    configurations { "Debug32", "Debug64", "Release32", "Release64" }
+    
+project "YAX.Math"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir "out/%{cfg.buildcfg}/%{cfg.platform}"
+    includedirs "include"
+    files "include/*.h"
+    files "src/*.cpp"
+    
+    flags "MultiProcessorCompile"
+    warnings "Extra"
+
+    filter "configurations:*32"
+        architecture "x86"
+    
+    filter "configurations:*64"
+        architecture "x86_64"
+        
+    filter "configurations:Debug*"
+        optimize "Off"
+         
+    filter "configurations:Release*"
+        floatingpoint "Strict"
+        optimize "Full"
